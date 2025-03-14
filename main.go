ec4034e7e73568a2ac93da995da006a09138c27f
Alekseev Konstantin <alexcseev1990@gmail.com>
2025-03-14 23:17:03 +0200

    func

--- main.go
+++ main.go
@@ -6,13 +6,26 @@ import (
 )
 
 func main() {
+	userHeight, userKg := getUserInput()
+	IMT := calculateIMT(userHeight, userKg)
+	outputResult(IMT)
+}
+
+func outputResult(IMT float64) {
+	fmt.Printf("IMT: %.0f \n", IMT)
+}
+
+func calculateIMT(height float64, weight float64) float64 {
 	const IMTPower = 2
+	return weight / math.Pow(height / 100, IMTPower)
+}
+
+func getUserInput() (float64, float64) {
 	var userHeight float64
 	var userKg float64
 	fmt.Print("Enter your height: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Enter your weight: ")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
-	fmt.Printf("IMT: %.0f", IMT)
+	return userHeight, userKg
 }
