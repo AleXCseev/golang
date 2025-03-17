cef7c4d58a10a046d880a18ab0ee6723bbc19b21
Konstantin Alekseev <alexcseev1990@gmail.com>
2025-03-17 16:11:21 +0200

    if

--- main.go
+++ main.go
@@ -5,10 +5,31 @@ import (
 	"math"
 )
 
+const IMTPower = 2
+
 func main() {
 	userHeight, userKg := getUserInput()
 	IMT := calculateIMT(userHeight, userKg)
 	outputResult(IMT)
+
+	if IMT < 16 {
+		fmt.Println("Сильный дефицит массы тела")
+	} else if IMT < 18.5 {
+		fmt.Println("Недостаточная масса тела")
+	} else if IMT < 25 {
+		fmt.Println("Норма")
+	} else if IMT < 30 {
+		fmt.Println("Избыточная масса тела")
+	} else if IMT < 35 {
+		fmt.Println("Ожирение первой степени")
+	} else if IMT < 40 {
+		fmt.Println("Ожирение второй степени")
+	} else if IMT >= 40 {
+		fmt.Println("Ожирение третьей степени")
+	} else {
+		fmt.Println("Неверные данные")
+		return
+	}
 }
 
 func outputResult(IMT float64) {
@@ -16,8 +37,7 @@ func outputResult(IMT float64) {
 }
 
 func calculateIMT(height float64, weight float64) float64 {
-	const IMTPower = 2
-	return weight / math.Pow(height / 100, IMTPower)
+	return weight / math.Pow(height/100, IMTPower)
 }
 
 func getUserInput() (float64, float64) {
